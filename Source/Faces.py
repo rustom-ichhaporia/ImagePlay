5c8ff9d693f1359ce2fab9f7f8393e278fb50547
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2020-09-18 09:41:06 -0500

    Changed path for MacOS

--- Source/Faces.py
+++ Source/Faces.py
@@ -1,9 +1,9 @@
-from Source import Colors
+# from Source import Colors
 import cv2
 
 unsplash_link = "https://api.unsplash.com/photos/?client_id=d0012cf0474c5276ab2c2a9db274aea9b0d23b81361435fa9a2e37bed8992bc9"
 
-cv2_directory = "C:/Users/Rustom Ichhaporia/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/cv2/data/"
+cv2_directory = "/Users/rustomichhaporia/opt/anaconda3/lib/python3.8/site-packages/cv2/data/"
 face_cascade = cv2.CascadeClassifier(cv2_directory+"haarcascade_frontalface_default.xml")
 face_image = cv2.imread("Images/Inputs/Group4.jpg")
 grey = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
